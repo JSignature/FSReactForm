060889a9941ccbc66f82bb1a91c6e9445669146e
Jimmy Signature <jimmysignature@gmail.com>
2023-07-26 21:36:16 -0400

    add Sign Up Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css'
+import Authenitcate from './components/Authenitcate'
+import SignUpForm from './components/SignUpForm'
 
 function App() {
-  return <></>
+  return (
+    <>
+      <SignUpForm />
+      <Authenitcate />
+    </>
+  )
 }
 
 export default App
