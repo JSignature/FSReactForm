ad8b698f05ef324361bf69cba3c3846de48a50f3
Jimmy Signature <jimmysignature@gmail.com>
2023-07-27 20:05:04 -0400

    add pending workshop text

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SignUpForm from './components/SignUpForm'
 function App() {
   return (
     <>
+      <h1>This worksop is not complete</h1>
       <SignUpForm />
       <Authenitcate />
     </>
