381407b5463d609235e8ba93b39c9ceb1fb67386
Jimmy Signature <jimmysignature@gmail.com>
2023-07-27 22:35:53 -0400

    authenicate error message set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import Authenitcate from './components/Authenitcate'
 import SignUpForm from './components/SignUpForm'
+import React, { useState } from 'react'
 
 function App() {
+  const [token, setToken] = useState(null)
+
   return (
     <>
       <h1>This workshop is not complete</h1>
-      <SignUpForm />
-      <Authenitcate />
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenitcate token={token} setToken={setToken} />
     </>
   )
 }
--- src/components/Authenitcate.jsx
+++ src/components/Authenitcate.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
 
-const Authenitcate = () => {
-  return <h2>Authenitcate</h2>
+const Authenitcate = ({ token }) => {
+  const [error, setError] = useState(null)
+
+  try {
+    async function handleClick() {
+      console.log('clicked')
+    }
+  } catch (error) {
+    setError(error.message)
+  }
+
+  return (
+    <>
+      <h2>Authenitcate</h2>
+      {error && <p>{error}</p>}
+      <button onClick={handleClick}>Authenticate Token</button>
+    </>
+  )
 }
 
 export default Authenitcate
