8af47d652623858fa1eee450594ac34523b08b68
Jimmy Signature <jimmysignature@gmail.com>
2023-07-27 20:07:24 -0400

    add pending workshop text spelling error

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import SignUpForm from './components/SignUpForm'
 function App() {
   return (
     <>
-      <h1>This worksop is not complete</h1>
+      <h1>This workshop is not complete</h1>
       <SignUpForm />
       <Authenitcate />
     </>
