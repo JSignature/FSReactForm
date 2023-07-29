420cb7725bca6f60e37d163e3405d6d97811f6c9
Jimmy Signature <jimmysignature@gmail.com>
2023-07-28 21:01:33 -0400

    add authenicate page

--- src/components/Authenitcate.jsx
+++ src/components/Authenitcate.jsx
@@ -3,18 +3,32 @@ import { useState } from 'react'
 
 const Authenitcate = ({ token }) => {
   const [error, setError] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null)
 
-  try {
-    async function handleClick() {
-      console.log('clicked')
+  async function handleClick() {
+    console.log('clicked', token)
+    try {
+      const response = await fetch(
+        'https://fsa-jwt-practice.herokuapp.com/authenticate',
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      const result = await response.json()
+      setSuccessMessage(result.message)
+    } catch (error) {
+      setError(error.message)
     }
-  } catch (error) {
-    setError(error.message)
   }
 
   return (
     <>
       <h2>Authenitcate</h2>
+      {successMessage && <p>{successMessage}</p>}
       {error && <p>{error}</p>}
       <button onClick={handleClick}>Authenticate Token</button>
     </>
